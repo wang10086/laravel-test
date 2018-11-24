8ab1a2713f1392ef86d93ddad3f0ba269cbf5861
wang10086 <1021056947@qq.com>
2018-11-24 23:29:47 +0800

    aa

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => null,
+            'engine' => 'InnoDB',
         ],
 
         'pgsql' => [
