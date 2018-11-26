c4ac6f4ada8c9638ee7d65713f6ff63b4c7af8ba
wang10086 <1021056947@qq.com>
2018-11-26 23:26:27 +0800

    aa

--- goods/resources/views/admin/admin/login.blade.php
+++ goods/resources/views/admin/admin/login.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body style="background:#F3F3F4;">
 	<div class="login_box">
-		<h2>欢迎使用XX管理平台</h2>
+		<h2>欢迎使用laravel管理平台</h2>
 		<div class="form" style="width:248px;">
 			@if(count($errors)>0)
 				@foreach($errors->all() as $error)
