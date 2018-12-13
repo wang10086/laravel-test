c39c1731ab0aa98152a4c88d10c1725a02e3ec56
wang10086 <1021056947@qq.com>
2018-12-13 22:24:24 +0800

    aa

--- laravel-admin/vendor/laravel-admin-ext/lock-screen/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Encore\Admin\LockScreen\Http\Controllers\LockScreenController;
-
-Route::get('auth/lock', LockScreenController::class.'@lock')->name('laravel-admin-lock');
-Route::post('auth/unlock', LockScreenController::class.'@unlock')->name('laravel-admin-unlock');
